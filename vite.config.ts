bb6a8fa86b71e22b55e2aabbfa7459b60425e9de
Wayface <wayfacee@gmail.com>
2025-01-04 00:32:32 +0600

    ff

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,8 @@ export default defineConfig({
   },
   define: {
     __IS_DEV__: JSON.stringify(true),
-    __API__: JSON.stringify("http://localhost:8000"),
+    // __API__: JSON.stringify("http://localhost:8000"),
+    __API__: JSON.stringify("https://news-profile-back.onrender.com"),
     __WEATHER_API__: JSON.stringify("https://api.openweathermap.org/data/2.5"),
     __CURRENCY_API__: JSON.stringify("https://api.exchangeratesapi.io"),
   },
